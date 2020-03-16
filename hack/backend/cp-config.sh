c7f63304bef7dc95f38b974974ebe1e1142da7c7
lhorvce <lhorvce@hotmail.com>
2020-03-16 18:46:53 +0800

    feat remove init.db file

--- hack/backend/cp-config.sh
+++ hack/backend/cp-config.sh
@@ -26,8 +26,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CONFIGS=("configs/backend/configs/config.default.yml" "configs/backend/.env" "configs/backend/api.local.env" "configs/backend/envs.conf" "configs/backend/initdb/init.sql")
-CONFIG_EXAMPLES=("configs/backend/configs/config.example.yml" "configs/backend/.env.example" "configs/backend/api.local.example.env" "configs/backend/envs.conf.example" "configs/backend/initdb/init.sql.example")
+CONFIGS=("configs/backend/configs/config.default.yml" "configs/backend/.env" "configs/backend/api.local.env" "configs/backend/envs.conf")
+CONFIG_EXAMPLES=("configs/backend/configs/config.example.yml" "configs/backend/.env.example" "configs/backend/api.local.example.env" "configs/backend/envs.conf.example")
 
 cpConfig(){
     i=0
